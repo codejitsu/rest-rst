7fc4333e9f827fa017208e940b56bdd2d56f9d13
codejitsu <me@codejitsu.net>
2021-02-13 14:00:29 +0100

    Add create course route

--- rest-rst/tutor-nodb/src/bin/tutor-service.rs
+++ rest-rst/tutor-nodb/src/bin/tutor-service.rs
@@ -1,6 +1,7 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, middleware, App, HttpServer};
 use std::io;
 use std::sync::Mutex;
+use env_logger;
 
 #[path = "../handlers.rs"]
 mod handlers;
@@ -19,6 +20,9 @@ use state::AppState;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let shared_data = web::Data::new(AppState {
         health_check_response: String::from("All good! I'm OK."),
         visit_count: Mutex::new(0),
@@ -26,7 +30,11 @@ async fn main() -> io::Result<()> {
     });
 
     let app = move || {
-        App::new().app_data(shared_data.clone()).configure(general_routes)
+        App::new()
+            .wrap(middleware::Logger::default())
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
     };
 
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
